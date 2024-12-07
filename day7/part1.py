cbc5fffe44b69d16de88e30928b6f5c410bd482f
Robert Reimann <34074906+MM1132@users.noreply.github.com>
2024-12-07 23:16:33 +0100

    day7

---
+++ day7/part1.py
@@ -0,0 +1,34 @@
+class Equation:
+	def __init__(self, answer, numbers):
+		self.answer = answer
+		self.numbers = numbers
+
+with open("input.txt") as f:
+	lines = f.read().strip()
+	lines = lines.split("\n")
+
+lines = [[v.strip() for v in line.split(":")] for line in lines]
+equations = [Equation(int(line[0]), list(map(int, line[1].split()))) for line in lines]
+
+def check_equation(equation, operators):
+	answer = equation.numbers[0]
+	for index in range(len(operators)):
+		if operators[index] == '+':
+			answer += equation.numbers[index + 1]
+		elif operators[index] == '*':
+			answer *= equation.numbers[index + 1]
+	return answer == equation.answer
+
+def is_solvable(equation, operators):
+	if len(operators) >= len(equation.numbers):
+		return False
+	if check_equation(equation, operators):
+		return True
+	return is_solvable(equation, operators + '+') or is_solvable(equation, operators + "*")
+
+
+total = 0
+for equation in equations:
+	if (is_solvable(equation, "")):
+		total += equation.answer
+print(total)
